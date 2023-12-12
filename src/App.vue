55f5f5d7250f666c795c37e1ff70ded99e30a492
peppegerace <peppegerace47@gmail.com>
2023-12-12 23:59:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import axios from "axios";
+import {store} from "./data/store";
+
+export default{
+  name: "App",
+  data(){
+    return{
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(store.apiUrl + "projects")
+        .then(results =>{
+          console.log(results.data);
+        })
+    }
+  },
+  mounted(){
+    this.getApi();
+  }
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>titolo</h1>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
